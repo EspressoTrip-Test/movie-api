5fd6e157a141ff5fafccd1e8c8bbcc4936b672b5
EspressoTrip-v2 <juanbo.home@gmail.com>
2021-08-12 10:12:50 +0200

    Comment

--- startup/prod.js
+++ startup/prod.js
@@ -1,6 +1,7 @@
 const compression = require('compression');
 const helmet = require('helmet');
 
+// Activate in production
 function production(app) {
   app.use(compression());
   app.use(helmet());
